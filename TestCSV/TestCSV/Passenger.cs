fff4145fd8ca084a8977528b32ac65e19bf30412
Sophie Bayliss <123488256+SophieB22@users.noreply.github.com>
2023-04-24 10:09:57 +0100

    Add files via upload

---
+++ TestCSV/TestCSV/Passenger.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCSV
+{
+    internal class Passenger
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int age { get; set; }
+        public string sex { get; set; }
+        public string occupation { get; set; }
+        public string nativeCountry { get; set; }
+        public string destination { get; set; }
+        public string embarked { get; set; }
+        public string chipID { get; set; }
+        public string arrivalDate { get; set; }
+
+
+        public Passenger()
+        {
+
+        }
+
+        public Passenger(string firstName)
+        {
+            this.firstName = firstName;
+        }
+        public string Name
+        {
+            get { return this.firstName; }
+            set { this.firstName = value; }
+        }
+
+        public string Last
+        {
+            get { return this.lastName; }
+            set { this.lastName = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+        public string Sex
+        {
+            get { return this.sex; }
+            set { this.sex = value; }
+        }
+
+        public string Occupation
+        {
+            get { return this.occupation; }
+            set { this.occupation = value; }
+        }
+
+        public string Nativecountry
+        {
+            get { return this.nativeCountry; }
+            set { this.nativeCountry= value; }
+        }
+
+        public string Destination
+        {
+            get { return this.destination; }
+            set { this.destination = value; }
+        }
+
+        public string Embarked
+        {
+            get { return this.embarked; }
+            set { this.embarked = value; }
+        }
+
+        public string ChipID
+        {
+            get { return this.chipID; }
+            set { this.chipID = value; }
+        }
+
+        public string ArrivalDate
+        {
+            get { return this.arrivalDate; }
+            set { this.arrivalDate = value; }
+        }
+    }
+
+    
+}
---
+++ TestCSV/TestCSV/Program.cs
@@ -0,0 +1,105 @@
+﻿namespace TestCSV
+{
+    internal class Program
+    {
+
+        static Passenger[] myPassenger = new Passenger[399];
+        static void Main(string[] args)
+        {
+            String filePath = @"C:\Users\sophi\OneDrive - Atlantic TU\MyPrograms\faminefiletoanalyse.csv";
+            try
+            {
+
+                StreamReader sr= new StreamReader(filePath);
+
+                string recordIn = sr.ReadLine();        //read first line to file
+                string[] fields = null;
+
+                int sum = 0;
+                int count = 0;
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        fields = line.Split(',');  // split where there is a comma
+                        Console.WriteLine(line);
+
+                        if(fields.Length ==3)
+                        {
+                            myPassenger[count] = new Passenger();
+                            myPassenger[count].firstName = fields[0];
+                            myPassenger[count].lastName = fields[1];
+
+                            int age = 0;
+                            myPassenger[count].age = age;
+                            if (fields[2].StartsWith("I"))  //Infant
+                                age = 0;
+                            if (fields[2].StartsWith("age"))
+                                age = int.Parse(fields[2].Substring(4, 2));  //Take out 4 charac, leave in 2(age)
+                            if (fields[2].StartsWith("U"))   //Unknown age
+                                age = -1;
+                            myPassenger[count].sex = fields[3];
+                            myPassenger[count].occupation = fields[4];
+                            myPassenger[count].nativeCountry = fields[5];
+                            myPassenger[count].destination = fields[6];
+                            myPassenger[count].embarked = fields[7];
+                            myPassenger[count].chipID = fields[8];
+                            myPassenger[count].arrivalDate = fields[9];
+
+                            count++;
+                        }
+                        recordIn = sr.ReadLine();  //read the next record
+
+                    }
+                    sr.Close();
+
+
+                    for(int i= 0; i < count; i++)
+                    {
+                        Console.WriteLine(myPassenger[i].lastName);
+                    }
+                }
+            }
+            catch(IOException myError)
+            {
+                Console.WriteLine(myError.Message);
+            }
+           catch(Exception myError)
+            {
+                Console.WriteLine("there was a problem");
+            }
+
+            // Main Menu
+
+            Console.WriteLine(" Main Menu ");
+            Console.WriteLine(" 1. Ship Reports ");
+            Console.WriteLine(" 2. Occupation Reports ");
+            Console.WriteLine(" 3. Age Reports ");
+            Console.WriteLine(" 4. Exit ");
+            Console.Write(" \nEnter Choice : ");
+
+            int menuChoice = int.Parse(Console.ReadLine());
+
+            while (menuChoice != 4)
+            {
+                if (menuChoice == 1)
+                {
+                    Console.WriteLine("Option 1 Selected ");
+                }
+
+                else if (menuChoice == 2)
+                {
+                    Console.WriteLine(" Option 2 Selected ");
+                }
+                else if (menuChoice == 3)
+                {
+                    Console.WriteLine(" Option 3 Selected");
+                }
+                else
+                    Console.WriteLine(" Invalid Choice ");
+            }
+        }
+            
+    }
+}
